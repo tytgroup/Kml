d749f452d4142b8308c579bccfbc5d2ffc63995b
Leo201407 <383760080@qq.com>
2014-08-21 16:12:28 +0800

    20140821

--- src/com/groupc/tyt/activity/Pub_Activity.java
+++ src/com/groupc/tyt/activity/Pub_Activity.java
@@ -43,12 +43,7 @@ public class Pub_Activity extends Activity{
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		ActionBar actionBar = getActionBar();
-		Resources r = getResources();
-		Drawable myDrawable = r.getDrawable(R.drawable.top_back);
-		actionBar.setBackgroundDrawable(myDrawable);
 		SpannableString spannableString = new SpannableString("取消发布");
-		spannableString.setSpan(new TypefaceSpan("monospace"), 0, spannableString.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-		spannableString.setSpan(new AbsoluteSizeSpan(24, true), 0, spannableString.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
 		getActionBar().setTitle(spannableString);
         actionBar.setDisplayHomeAsUpEnabled(true);
 		setContentView(R.layout.publish);
